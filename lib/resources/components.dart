46fa4b2b20610d9d168a4e52d0dd7372638069f3
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2023-05-14 12:41:59 +0300

    Update components.dart

--- lib/resources/components.dart
+++ lib/resources/components.dart
@@ -22,4 +22,61 @@ class Components {
       barrierDismissible: false,
     );
   }
+  
+  
+//   Future<bool> getCurrentLocation() async {
+//     bool serviceEnabled;
+//     LocationPermission permission;
+
+//     serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//     if (!serviceEnabled) {
+//       Get.defaultDialog(
+//         title: 'Location Service Disabled',
+//         middleText: 'Go to Settings And Enable Location Service.',
+//         actions: [
+//           TextButton(
+//             onPressed: () async {
+//               Get.back();
+//               await Geolocator.openLocationSettings();
+//             },
+//             child: const Text('Open Location Settings'),
+//           ),
+//         ],
+//       );
+
+//       return false;
+//     }
+//     permission = await Geolocator.checkPermission();
+//     if (permission == LocationPermission.denied) {
+//       permission = await Geolocator.requestPermission();
+//       if (permission == LocationPermission.deniedForever) {
+//         Get.defaultDialog(
+//           title: 'Location Permission Denied',
+//           middleText:
+//               'Go to Settings > Apps > Loyalty User > Permissions.\n\nAnd Enable Location Service.',
+//           actions: [
+//             TextButton(
+//               onPressed: () async {
+//                 Get.back();
+//                 await Geolocator.openAppSettings();
+//               },
+//               child: const Text('Open Settings'),
+//             ),
+//           ],
+//         );
+//         return false;
+//       }
+//       if (permission == LocationPermission.denied) {
+//         return false;
+//       }
+//     }
+
+//     currentPosition = await Geolocator.getCurrentPosition(
+//         desiredAccuracy: LocationAccuracy.high);
+
+//     return true;
+//   }
+
+  
+  
 }
