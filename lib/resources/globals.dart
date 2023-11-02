b45f9235bd44f7e01240c9a6a14c7a91ad9d702d
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2023-11-02 15:54:30 +0300

    add a lot of features

--- lib/resources/globals.dart
+++ lib/resources/globals.dart
@@ -1,12 +1,5 @@
-import '../models/auth_model.dart';
-
 class Globals {
   Globals._();
+
   static final Globals instance = Globals._();
-
-  AuthModel? authModel;
-
-  void changeAuthModel(AuthModel value) {
-    authModel = value;
-  }
 }
