8c32070352c067e2b5935c48f1087e4e2468f252
Sohib Al-Bakri <sohibbakri9@gmail.com>
2023-11-09 11:31:02 +0300

    fix code

--- lib/resources/theme/colors_manager.dart
+++ lib/resources/theme/colors_manager.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ColorsManager {
+  //* Light Theme Colors
   static Color primary = const Color(0xffED9728);
+  static const Color secondary = Color(0xff813DE8);
   static Color background = const Color(0xffFFFFFF);
   static Color white = const Color(0xffFFFFFF);
   static Color black = const Color(0xff000000);
-  static Color grey = const Color(0xff808080);
+  static Color grey = const Color(0xff656565);
+  static const Color divider = Color(0xffD0D7DE);
+  static const Color textField = Color(0xffF2F5FC);
+  static const Color pink = Color(0xffF262BA); //chick if used
+  static const Color blue = Color(0xff11A2E3); //chick if used
+  static const Color text = Color(0xFF3F3F3F); //chick if used
+  static const Color sender = Color(0xffED9728); //chick if used
+  static const Color receiver = Color(0xFF5d5d5d); //chick if used
 
-  // Dark
+  //* Dark Theme Colors
   static Color darkPrimary = const Color(0xffED9728);
-  static Color darkBackground = const Color(0xff000000);
+  static const Color darkecondary = Color.fromARGB(255, 253, 184, 95);
+  static Color darkBackground = const Color(0xff1F1F1F);
   static Color darkWhite = const Color(0xff000000);
   static Color darkBlack = const Color(0xffFFFFFF);
-  static Color darkGrey = const Color(0xff5A5A5A);
+  static Color darkGrey = const Color(0xff656565);
+  static const Color darkDivider = Color(0xff30363D);
+  static const Color darkTextField = Color(0xFF323337);
+  static const Color darkText = Color(0xFFE5E5E5); //chick if used
 
   static const Color success = Color(0xFF4BB543);
-  static const Color error = Color(0xFFFF3333);
   static const Color warning = Color(0xFFffcc00);
+  static const Color error = Color(0xFFFF3333);
   static const Color snackBarBackground = Color(0xFF303030);
 }
