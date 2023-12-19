636f20e74dc59e87328e137d36186682a09ee06d
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2023-12-19 09:20:51 +0300

    update

--- lib/resources/managers/constants_manager.dart
+++ lib/resources/managers/constants_manager.dart
@@ -1,18 +1,18 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ConstantsManager {
-  static const String appStoreId = 'appStoreId';
   static const String arabicLangValue = 'ar';
   static const String englishLangValue = 'en';
   static const int genderMaleValue = 1;
   static const int genderFemaleValue = 2;
   static const double appbarHeight = 50.0;
 
+  static const String appStoreId = 'appStoreId';
   static const String googlePlayLink =
       'https://play.google.com/store/apps/details?id=com.example.start_up_workspace';
   static const String appStoreLink = 'http://apps.apple.com/<country>/app/<app–name>/id<store-ID>';
   static const String shareText =
-      'Introducing \'App Name\' app – your pocket companion for mental wellness. If you or someone you care about could use support, download now from Google Play and the App Store.\nAndroid Edition: $googlePlayLink\nIOS Edition: $appStoreLink';
+      'Introducing \'App Name\' app . If you or someone you care about could use support, download now from Google Play and the App Store.\nAndroid Edition: $googlePlayLink\nIOS Edition: $appStoreLink';
   static const String termsAndConditionsUrl = 'https://google.com';
 }
 
