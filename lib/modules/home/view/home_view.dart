d2906f6a88fdd99cf7d616f5b43f1d47ca3d90f6
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2024-01-04 15:40:39 +0300

    exports

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -14,25 +14,6 @@ class HomeView extends StatelessWidget {
         title: const Text('Home'),
       ),
       drawer: const DrawerHome(),
-      body: Center(
-        child: FilledButton(
-          onPressed: () async {
-            try {
-              final ImagePicker picker = ImagePicker();
-              final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-              if (image == null) return;
-              if (context.mounted) {
-                final CroppedFile? croppedFile =
-                    await Components.cropImage(context, path: image.path);
-                print(croppedFile);
-              }
-            } catch (e) {
-              rethrow;
-            }
-          },
-          child: const Text('Pick'),
-        ),
-      ),
     );
   }
 }
